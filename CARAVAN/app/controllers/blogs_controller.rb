0886ba305af3d884a2eca45a311e7e6063f00c2e
加藤裕介 <morsprincipiumest11@gmail.com>
2020-12-28 13:26:25 +0900

    [Finish]投稿データの削除機能を追加

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   
   def show
     @blog = Blog.find(params[:id])
-  end 
+  end
   
   def index
     @blogs = Blog.all  
@@ -28,6 +28,13 @@ class BlogsController < ApplicationController
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+  
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
